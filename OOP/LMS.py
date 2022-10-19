90259b9650d79a002da6c6515d9332bfbfca5967
[omkar shinde] <omkar.shinde@everestek.com>
2022-10-19 16:41:00 +0530

    added LMS.py

--- OOP/LMS.py
+++ OOP/LMS.py
@@ -1,4 +1,4 @@
-'''
+"""
 You have to build a library management system using Django where an admin can
 perform CRUD ( create, read, update and delete) operations like
     ● Add a Book
@@ -27,5 +27,39 @@ Conditions:
     2. Write the documentation for backend code
 
 
-'''
+"""
 
+
+def ask_details(func):
+    std_id = input("Enter student ID: ")
+    std_name = input("Enter student Name: ")
+    std_class = input("Enter student class: ")
+    std_div = input("Enter student division: ")
+    std_contact = int(input("Enter student contact: "))
+
+
+# def print_details():
+#     ask_details()
+#     return ask_details
+
+
+# print_details = ask_details(print_details)
+
+
+@ask_details
+def print_details():
+    ask_details()
+
+# def std_info(func): 
+#     print(f"""
+#             Student ID: {s.std_id},
+#             Student Name: {s.std_name},
+#             Student Class: {s.std_class},
+#             Student Division: {s.std_div},
+#             Student Contact: {s.std_contact}
+#             """
+#             )
+
+# @std_info
+# def add_std_info():
+#     std_info()
